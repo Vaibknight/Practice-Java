3aff697d56435c0338c2f77d91d82d0b0bc81dde
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-09-26 22:16:02 +0530

    made 2 class

--- vaibhav/src/vaibhav/PracticeJava.java
+++ vaibhav/src/vaibhav/PracticeJava.java
@@ -3,8 +3,27 @@ package vaibhav;
 public class PracticeJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Pen p1 = new Pen();
+		Pen p2 = new Pen();
+		
+		p1.setColor("green");
+		p2.setColor("blue");
+		p1.setTip(45);
+		System.out.println(p1.tip);
 
 	}
 
+}
+
+class Pen {
+	String color;
+	int tip;
+	
+	void setColor(String newColor) {
+		color = newColor;
+	}
+	
+	void setTip(int newTip) {
+		tip = newTip;
+	}
 }
