2ecf7ef2bb5fdeab86161a247fa537fd3c1986a2
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-09-26 22:52:54 +0530

    get or set

--- vaibhav/src/vaibhav/PracticeJava.java
+++ vaibhav/src/vaibhav/PracticeJava.java
@@ -8,17 +8,29 @@ public class PracticeJava {
 		
 		p1.setColor("green");
 		p2.setColor("blue");
-		p1.setTip(45);
-		System.out.println(p1.tip);
+		p1.setTip(99);
+		p2.setTip(34);
+		System.out.println(p1.getColor());
+		System.out.println(p2.getColor());
+		System.out.println(p1.getTip());
+		System.out.println(p2.getTip());
 
 	}
 
 }
 
 class Pen {
-	String color;
-	int tip;
+	private String color;
+	private int tip;
 	
+	
+	String getColor() {
+		return this.color;
+	}
+	
+	int getTip() {
+		return this.tip;
+	}
 	void setColor(String newColor) {
 		color = newColor;
 	}
