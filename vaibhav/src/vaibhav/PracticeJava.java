75d4b96c96e5785ed752d439999c71294e2cbec1
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-09-27 23:35:11 +0530

    inherit

--- vaibhav/src/vaibhav/PracticeJava.java
+++ vaibhav/src/vaibhav/PracticeJava.java
@@ -3,60 +3,31 @@ package vaibhav;
 public class PracticeJava {
 
 	public static void main(String[] args) {
-		Student st = new Student();
 		
-		st.Name = "Vaibhav";
-		st.Roll = 4444;
-		st.pass = "hh";
-		System.out.println(st.Name+ " "+st.Roll);
-		st.Marks[0] = 120;
-		st.Marks[1] = 50;
-		st.Marks[2] = 60;
-		
-		Student st2 = new Student(st);
-		st.Marks [2] = 200;
-		st2.pass = "ii";
-		
-		
-		for(int i=0; i<3; i++) {
-			System.out.println(st2.Marks[i]);
-		}
-		
+		Fish dolphin = new Fish();
+		dolphin.eat();
 	}
 	
 }
 
-class Student {
+class Animal {
+	String color;
 	
-	String Name;
-	int Roll;
-	String pass;
-	int Marks[];
 	
-	Student(Student st){
-		Marks  = new int[3];
-		this.Name = st.Name;
-		this.Roll = st.Roll;
-		for(int i=0; i<3; i++) {
-			this.Marks[i] = st.Marks[i];
-		}
-		
+	void eat() {
+		System.out.println("eat");
 	}
 	
-	Student (){
-		Marks  = new int[3];
-		System.out.println("KKK");
-		
+	void breathe() {
+		System.out.println("breathes");
 	}
+}
+
+class Fish extends Animal{
 	
-	Student(String Name){
-		Marks  = new int[3];
-		this.Name = Name;
+	int fin;
+	
+	void swim() {
+		System.out.println("swim");
 	}
-	
-	Student(int Roll){
-		Marks  = new int[3];
-		this.Roll = Roll;
-	}
-	
 }
