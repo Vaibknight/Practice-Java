efaeff0ba19210bd108a411436e0b7812dff9a41
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-10-03 21:49:27 +0530

    repeating

--- vaibhav/src/vaibhav/Strings.java
+++ vaibhav/src/vaibhav/Strings.java
@@ -2,33 +2,30 @@ package vaibhav;
 
 public class Strings {
 	
-public static boolean isAnagram(String s, String t) {
-        
-        if(s.length() == t.length()){
-            return true;
-        }
-        
-        int arr[] = new int[26];
-        
-        for(int i=0; i<s.length(); i++){
-            ++arr[s.charAt(i) - 'a'];
-            --arr[s.charAt(i) - 'a'];
-        }
-        
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] > 0){
-                return false;
-            }
-        }
-        return true;
-    }
+ public static String parent(String s) {
+	 int n = s.length();
+	 String res = "";
+	 
+	 for(int i=0; i<s.length(); i++) {
+		 
+		 if(i<n-1 && s.charAt(i)== s.charAt(i+1)) {
+			 
+			 continue;
+		 }
+		 
+		 else {
+			 res= res+s.charAt(i);
+		 }
+	 }
+	 
+	 return res;
+	 
+ }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "anagram";
-		String  t = "nagaram";
-		
-		System.out.println(isAnagram(s,t));
+		String s = "aabb";
+		System.out.println(parent(s));
 	}
 
 }
