4dfd72580b5323c77a12b872ee9ff2612e504a9e
Vaibhav Tiwari <tiwarivaibhav1410@gmail.com>
2022-10-04 21:02:47 +0530

    long

---
+++ vaibhav/src/vaibhav/LongestCommonn.java
@@ -0,0 +1,36 @@
+package vaibhav;
+
+import java.util.*;
+
+public class LongestCommonn {
+	
+	
+	 public static String longestCommonPrefix(String[] strs) {
+	        
+	        StringBuilder s = new StringBuilder();
+	        
+	        Arrays.sort(strs);
+	        
+	        char[] first  = strs[0].toCharArray();
+	        char[] last  = strs[strs.length-1].toCharArray() ;
+	        
+	        
+	        for(int i=0 ; i<first.length-1; i++){
+	            if(first[i] != last[i])
+	                break;
+	            
+	            s.append(first[i]);
+	        }
+	        return s.toString();
+	    }
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String strs [] = {"flower","flow","flight"};
+		
+		System.out.println(longestCommonPrefix(strs));
+
+	}
+
+}
